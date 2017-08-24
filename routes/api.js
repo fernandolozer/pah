d6713bdc70987d45dbc7e0fef5d0c6109aa8ee81
Fernando Lozer Lichtenheld <flichtenheld@gmail.com>
2017-08-24 14:24:57 +0200

    Another change to make it compatible with slack

--- routes/api.js
+++ routes/api.js
@@ -12,10 +12,14 @@ router.get('/', (req, res) => {
 
 // Slack sends a post event
 router.post('/', (req, res) => {
-  let pah = '(\\____/)\n' +
-      '( ͡ ⚫͜ ʖ͡⚫️\n' +
-      '  \\╭👉   \\👉';
-  res.status(200).send(pah);
+
+    let response = {
+        response_type: 'in_channel',
+        text: '(\\____/)\n' +
+            '( ͡ ⚫͜ ʖ͡⚫️\n' +
+            '  \\╭👉   \\👉'
+    };
+    res.status(200).send(response);
 });
 
 module.exports = router;
